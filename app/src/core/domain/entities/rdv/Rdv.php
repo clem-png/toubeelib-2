6f220e289b9acac99c9e01cd5e7d0653c79f27fd
Aliec <amaury.quilliec@icloud.com>
2024-09-10 17:08:23 +0200

    ajout Entité RDV

---
+++ app/src/core/domain/entities/rdv/Rdv.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace toubeelib\core\domain\entities\praticien;
+
+use toubeelib\core\domain\entities\Entity;
+use toubeelib\core\dto\RdvDTO;
+
+class Rdv extends Entity
+{
+    protected string $idPatient;
+    protected string $idPraticient;
+    protected DateTime $dateDebut;
+    protected DateTime $dateFin;
+    protected Specialite $specialite;
+    protected string $status;
+
+    public function __construct(string $idPatient, string $idPraticient, string $status, DateTime $dateDebut, DateTime $dateFin, Specialite $spe)
+    {
+        $this->idPatient = $idPatient;
+        $this->idPraticient = $idPraticient;
+        $this->dateDebut = $dateDebut;
+        $this->dateFin = $dateFin;
+        $this->specialite = $spe;
+        $this->status = $status;
+    }
+
+    public function toDTO(): RdvDTO
+    {
+        return new RdvDTO($this);
+    }
+}
