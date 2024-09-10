40646244be667b2aaada99094237cf59b0c98045
clem.png <clem.netange@gmail.com>
2024-09-10 17:55:08 +0200

    Debut ServiceRDV

---
+++ app/src/core/services/rdv/ServiceRDVInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace toubeelib\core\services\rdv;
+
+use toubeelib\core\dto\InputPraticienDTO;
+use toubeelib\core\dto\InputRdvDTO;
+use toubeelib\core\dto\PraticienDTO;
+use toubeelib\core\dto\RdvDTO;
+use toubeelib\core\dto\SpecialiteDTO;
+
+interface ServiceRDVInterface{
+
+    public function listerDisponibilitesPraticien();
+    public function consulterRdv(string $rdv_id);
+    public function creerRdv(InputRdvDTO $DTO);
+    public function annulerRdv();
+    public function modifierPatientRdv();
+
+}
---
+++ app/src/core/services/rdv/ServiceRdv.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace toubeelib\core\services\rdv;
+
+use PharIo\Manifest\Exception;
+use toubeelib\core\dto\InputRdvDTO;
+use toubeelib\core\repositoryInterfaces\RdvRepositoryInterface;
+
+class ServiceRdv implements ServiceRDVInterface{
+
+    private RdvRepositoryInterface $rdvRepository;
+
+    public function __construct(RdvRepositoryInterface $rdvRepository){
+        $this->rdvRepository = $rdvRepository;
+    }
+
+    public function consulterRdv(string $rdv_id){
+        try {
+            $rdv = $this->rdvRepository->getRdvById($rdv_id);
+        }catch (Exception $e){
+            throw new \Exception($e);
+        }
+
+        $rdvDTO = $rdv->toDTO();
+        // TODO : FAIRE LES EXCEPTIONS ET VERIFICATION
+        return $rdvDTO;
+    }
+
+    public function listerDisponibilitesPraticien(){
+        // TODO: Implement listerDisponibilitesPraticien() method.
+    }
+
+    public function creerRdv(InputRdvDTO $DTO){
+        // TODO: Implement creerRdv() method.
+    }
+
+    public function annulerRdv(){
+        // TODO: Implement annulerRdv() method.
+    }
+
+    public function modifierPatientRdv(){
+        // TODO: Implement modifierPatientRdv() method.
+    }
+}
