6637ddff357eb0012d2aca47a238230b838e1722
etique5u <ketique04@gmail.com>
2024-09-10 17:50:13 +0200

    ArrayRdvRepository debut implementation

--- app/src/infrastructure/repositories/ArrayRdvRepository.php
+++ app/src/infrastructure/repositories/ArrayRdvRepository.php
@@ -3,6 +3,8 @@
 namespace toubeelib\infrastructure\repositories;
 
 use Ramsey\Uuid\Uuid;
+use toubeelib\core\domain\entities\praticien\Specialite;
+use toubeelib\core\domain\entities\rdv\Rdv;
 use toubeelib\core\domain\entities\rdv\RendezVous;
 use toubeelib\core\repositoryInterfaces\RdvRepositoryInterface;
 use toubeelib\core\repositoryInterfaces\RepositoryEntityNotFoundException;
@@ -12,15 +14,32 @@ class ArrayRdvRepository implements RdvRepositoryInterface
     private array $rdvs = [];
 
     public function __construct() {
-            $r1 = new RendezVous('p1', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 09:00') );
+            $r1 = new Rdv('p1', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 09:00') );
             $r1->setID('r1');
-            $r2 = new RendezVous('p1', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 10:00'));
+            $r2 = new Rdv('p1', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 10:00'));
             $r2->setID('r2');
-            $r3 = new RendezVous('p2', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 09:30'));
+            $r3 = new Rdv('p2', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 09:30'));
             $r3->setID('r3');
 
         $this->rdvs  = ['r1'=> $r1, 'r2'=>$r2, 'r3'=> $r3 ];
     }
 
-  
+
+    public function getRdvById(string $id): Rdv
+    {
+        if (!array_key_exists($id, $this->rdvs)) {
+            throw new RepositoryEntityNotFoundException('Rdv not found');
+        }
+        return $this->rdvs[$id];
+    }
+
+    public function save(Rdv $rdv): string
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function getSpecialiteById(string $id): Specialite
+    {
+        // TODO: Implement getSpecialiteById() method.
+    }
 }
