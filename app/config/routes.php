901494245fbddfccc78519964abc7af351b063de
4n0m4lie <mathias.ringot@gmail.com>
2024-09-17 16:33:15 +0200

    Début routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,11 +3,13 @@ declare(strict_types=1);
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use toubeelib\application\actions\GetRdvsByIdAction;
 
 return function( \Slim\App $app):\Slim\App {
 
     $app->get('/', \toubeelib\application\actions\HomeAction::class);
 
+    $app->get('/rdvs/{ID-RDV}[/]', GetRdvsByIdAction::class)->setName('rdvsId');
 
     return $app;
 };
---
+++ app/src/application/actions/GetRdvsByIdAction.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace toubeelib\application\actions;
+
+class GetRdvsByIdAction
+{
+
+}
