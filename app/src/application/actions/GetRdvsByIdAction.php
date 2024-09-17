47239f7febfdddb039848b9b98a987aa3851f780
4n0m4lie <mathias.ringot@gmail.com>
2024-09-17 16:52:12 +0200

    Début routes

--- app/src/application/actions/GetRdvsByIdAction.php
+++ app/src/application/actions/GetRdvsByIdAction.php
@@ -4,16 +4,29 @@ namespace toubeelib\application\actions;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use toubeelib\core\services\rdv\RdvServiceException;
+use toubeelib\core\services\rdv\ServiceRdv;
+use toubeelib\core\services\rdv\ServiceRDVInterface;
+
 
 class GetRdvsByIdAction extends AbstractAction
 {
 
+    private ServiceRDVInterface $serviceRdv;
 
+    public function __construct(ServiceRdv $serviceRdv)
+    {
+        $this->serviceRdv = $serviceRdv;
+    }
+
+
+    /**
+     * @throws RdvServiceException
+     */
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
     {
         $id = $args['ID-RDV'];
+        $rdv = $this->serviceRdv->consulterRdv($id);
 
-
-        // TODO: Implement __invoke() method.
     }
 }
