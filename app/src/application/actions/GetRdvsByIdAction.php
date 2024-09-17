68fe7843bf55a60a780957d62cae6d6cf955aa3c
etique5u <ketique04@gmail.com>
2024-09-17 16:40:40 +0200

    pour mathias

--- app/src/application/actions/GetRdvsByIdAction.php
+++ app/src/application/actions/GetRdvsByIdAction.php
@@ -2,7 +2,18 @@
 
 namespace toubeelib\application\actions;
 
-class GetRdvsByIdAction
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class GetRdvsByIdAction extends AbstractAction
 {
 
+
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
+    {
+        $id = $args['ID-RDV'];
+
+
+        // TODO: Implement __invoke() method.
+    }
 }
