9d91463f4a126dd8c52b15cfa4db6e058db83fb2
4n0m4lie <mathias.ringot@gmail.com>
2024-10-08 14:24:06 +0200

    utilisation de namespace dans settings.php

--- app/config/settings.php
+++ app/config/settings.php
@@ -1,6 +1,8 @@
 <?php
 
 use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
 use Psr\Container\ContainerInterface;
 
 return  [
@@ -8,10 +10,10 @@ return  [
     'displayErrorDetails' => true,
     'logs.dir' => __DIR__ . '/../var/logs',
     'logs.name' => 'toubeelib.log',
-    'logs.level' => \Monolog\Level::Info,
+    'logs.level' => Level::Info,
 
     'logger' => function( ContainerInterface $c) {
-        $log = new \Monolog\Logger( $c->get('logs.name'));
+        $log = new Logger( $c->get('logs.name'));
         $log->pushHandler(
             new StreamHandler($c->get('logs.dir'),
                 $c->get('logs.level')));
