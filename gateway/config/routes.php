6e2adfad95b0c4f0000aef46e588045bc820d2e3
EtiqueKevin <ketique04@gmail.com>
2025-01-08 16:54:15 +0100

    init de la gateway

---
+++ gateway/config/routes.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\App;
+
+
+return function( App $app): App {
+
+    return $app;
+};
