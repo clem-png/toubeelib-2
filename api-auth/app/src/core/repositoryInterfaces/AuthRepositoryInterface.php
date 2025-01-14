0231625df078b8ad1c5ad847857822567d90f7d1
Aliec-AQ <amaury.quilliec@icloud.com>
2025-01-14 10:29:30 +0100

    ajout userservice

--- api-auth/app/src/core/repositoryInterfaces/AuthRepositoryInterface.php
+++ api-auth/app/src/core/repositoryInterfaces/AuthRepositoryInterface.php
@@ -6,4 +6,7 @@ use toubeelib_auth\core\domain\entities\auth\Auth;
 
 interface AuthRepositoryInterface
 {
-    function findByEmail(string $email):Auth;}
+    function findByEmail(string $email):Auth;
+    function save(Auth $auth): string;
+    function findById(string $id):Auth;
+}
